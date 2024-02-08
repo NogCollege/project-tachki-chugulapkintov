de45fe3b4b71647449d11a673bfc9b4bcefc1440
Qokk <146724110+Joakarr@users.noreply.github.com>
2024-02-08 14:23:02 +0300

    080220204
    sql

--- templates/grid.php
+++ templates/grid.php
@@ -48,193 +48,23 @@
             
         </div>
     </div>
-            <!-- <div class="block__main__grid__zxc">
+            <div class="block__main__grid__zxc">
                 <div class="container">
 
-                <div class="block__grid"> -->
+                <!-- <div class="block__grid"> -->
                     <?php
                     require_once 'controllers/connect.php';
                         foreach ($data as $elem){
-                            $result.="<div class='block__main__grid__zxc'>"."<div class='block__grid'>"."<p class='p__zxc__grid'>".$elem['city']."</p>"."<img src='templates\img\image 22.png'>"."<h3>".$elem['full_name'].' '.$elem['year']."</h3>"."<p>"."<img class='logo_card' src='templates\img\qng.png'>".$elem['engin_capacity']." ".$elem['engin']."<img class='logo_card' src='templates\img\hp.png'>".$elem['horse']."</p>".$elem['cost_max']."</div>"."</div>".'<br>';
+                            $result.="<div class='block__grid'>"."<p class='p__zxc__grid'>".$elem['city']."</p>"."<img src='templates\img\image 22.png'>"."<p class='p__zxc'>".$elem['full_name'].' '.$elem['year']."</p>"."<div class = 'about'>"."<p>"."<img class='logo_card' src='templates\img\Vector (2).png'>".$elem['engin_capacity']." ".$elem['engin']."</p>"."<p>"."<img class='logo_card' src='templates\img\Vector (3).png'>".$elem['horse']."</p>"."</div>"."<hr>"."<div class='inf'>"."<a href=''>"."<div class='aaaaapen'"."Забронтровать"."</div>"."</a>"."<div class='ppppen'>"."".$elem['cost_max']."</div>";
                         }
                         echo $result;
                         ;
 
                     ?>
-                <!-- </div> -->
-
-                <!-- <div class="block__grid">
-                    <div class="p__zxc__grid">
-                    <p class="p__grid"><??></p>
-                </div>
-                    <img class="penus" src="img/image 22.png" alt="">
-                    <p class="p__zxc">BMW X2, 2021</p>
-                    <div class="about">
-                        
-                        <p> <img src="img/Vector (2).png" alt=""> 2,3 л./бензин</p>
-                        
-                        <p><img src="img/Vector (3).png" alt=""> 317 л.с</p>
-                    </div>
-                    <hr>
-                    <div class="inf">
-                        <a href=""><div class="aaaaapen">Забронировать</div></a>
-                        <div class="ppppen"><p>от</p> <h3>7 150</h3> <p>руб/сут.</p></div>
+               
     
                     </div>
                 </div>
-                <div class="block__grid">
-                    <div class="p__zxc__grid">
-                    <p class="p__grid">дубай</p>
-                </div>
-                    <img class="penus" src="img/image 22 (1).png" alt="">
-                    <p class="p__zxc">Kia Sportage, 2022</p>
-                    <div class="about">
-                        
-                        <p> <img src="img/Vector (2).png" alt=""> 2,3 л./бензин</p>
-                        
-                        <p><img src="img/Vector (3).png" alt=""> 317 л.с</p>
-                    </div>
-                    <hr>
-                    <div class="inf">
-                        <div class="aaaaapen"><a href="">Забронировать</a></div>
-                        <div class="ppppen"><p>от</p> <h3>7 150</h3> <p>руб/сут.</p></div>
-    
-                    </div>
-                </div>
-                <div class="block__grid">
-                    <div class="p__zxc__grid">
-                    <p class="p__grid">дубай</p>
-                </div>
-                    <img class="penus" src="img/image 22 (2).png" alt="">
-                    <p class="p__zxc">Lexus IS, 2018</p>
-                    <div class="about">
-                        
-                        <p> <img src="img/Vector (2).png" alt=""> 2,3 л./бензин</p>
-                        
-                        <p><img src="img/Vector (3).png" alt=""> 317 л.с</p>
-                    </div>
-                    <hr>
-                    <div class="inf">
-                        <div class="aaaaapen"><a href="">Забронировать</a></div>
-                        <div class="ppppen"><p>от</p> <h3>7 150</h3> <p>руб/сут.</p></div>
-    
-                    </div>
-                </div>
-                <div class="block__grid">
-                    <div class="p__zxc__grid">
-                    <p class="p__grid">дубай</p>
-                </div>
-                    <img class="penus" src="img/image 22 (3).png" alt="">
-                    <p class="p__zxc">Jaguar XF,  2012</p>
-                    <div class="about">
-                        
-                        <p> <img src="img/Vector (2).png" alt=""> 2,3 л./бензин</p>
-                        
-                        <p><img src="img/Vector (3).png" alt=""> 317 л.с</p>
-                    </div>
-                    <hr>
-                    <div class="inf">
-                        <div class="aaaaapen"><a href="">Забронировать</a></div>
-                        <div class="ppppen"><p>от</p> <h3>7 150</h3> <p>руб/сут.</p></div>
-    
-                    </div>
-                </div>
-                <div class="block__grid">
-                    <div class="p__zxc__grid">
-                    <p class="p__grid">дубай</p>
-                </div>
-                    <img class="penus" src="img/image 22 (4).png" alt="">
-                    <p class="p__zxc">Toyota RAV4,  2017</p>
-                    <div class="about">
-                        
-                        <p> <img src="img/Vector (2).png" alt=""> 2,3 л./бензин</p>
-                        
-                        <p><img src="img/Vector (3).png" alt=""> 317 л.с</p>
-                    </div>
-                    <hr>
-                    <div class="inf">
-                        <div class="aaaaapen"><a href="">Забронировать</a></div>
-                        <div class="ppppen"><p>от</p> <h3>7 150</h3> <p>руб/сут.</p></div>
-    
-                    </div>
-                </div>
-                <div class="block__grid">
-                    <div class="p__zxc__grid">
-                    <p class="p__grid">дубай</p>
-                </div>
-                    <img class="penus" src="img/image 22.png" alt="">
-                    <p class="p__zxc">BMW X2, 2021</p>
-                    <div class="about">
-                        
-                        <p> <img src="img/Vector (2).png" alt=""> 2,3 л./бензин</p>
-                        
-                        <p><img src="img/Vector (3).png" alt=""> 317 л.с</p>
-                    </div>
-                    <hr>
-                    <div class="inf">
-                        <div class="aaaaapen"><a href="">Забронировать</a></div>
-                        <div class="ppppen"><p>от</p> <h3>7 150</h3> <p>руб/сут.</p></div>
-    
-                    </div>
-                </div>
-                <div class="block__grid">
-                    <div class="p__zxc__grid">
-                    <p class="p__grid">дубай</p>
-                </div>
-                    <img class="penus" src="img/image 22 (1).png" alt="">
-                    <p class="p__zxc">Kia Sportage, 2022</p>
-                    <div class="about">
-                        
-                        <p> <img src="img/Vector (2).png" alt=""> 2,3 л./бензин</p>
-                        
-                        <p><img src="img/Vector (3).png" alt=""> 317 л.с</p>
-                    </div>
-                    <hr>
-                    <div class="inf">
-                        <div class="aaaaapen"><a href="">Забронировать</a></div>
-                        <div class="ppppen"><p>от</p> <h3>7 150</h3> <p>руб/сут.</p></div>
-    
-                    </div>
-                </div>
-                <div class="block__grid">
-                    <div class="p__zxc__grid">
-                    <p class="p__grid">дубай</p>
-                </div>
-                    <img class="penus" src="img/image 22 (2).png" alt="">
-                    <p class="p__zxc">Lexus IS, 2018</p>
-                    <div class="about">
-                        
-                        <p> <img src="img/Vector (2).png" alt=""> 2,3 л./бензин</p>
-                        
-                        <p><img src="img/Vector (3).png" alt=""> 317 л.с</p>
-                    </div>
-                    <hr>
-                    <div class="inf">
-                        <div class="aaaaapen"><a href="">Забронировать</a></div>
-                        <div class="ppppen"><p>от</p> <h3>7 150</h3> <p>руб/сут.</p></div>
-    
-                    </div>
-                </div>
-                <div class="block__grid">
-                    <div class="p__zxc__grid">
-                    <p class="p__grid">дубай</p>
-                </div>
-                    <img class="penus" src="img/image 22 (3).png" alt="">
-                    <p class="p__zxc">Jaguar XF,  2012</p>
-                    <div class="about">
-                        
-                        <p> <img src="img/Vector (2).png" alt=""> 2,3 л./бензин</p>
-                        
-                        <p><img src="img/Vector (3).png" alt=""> 317 л.с</p>
-                    </div>
-                    <hr>
-                    <div class="inf">
-                        <div class="aaaaapen"><a href="">Забронировать</a></div>
-                        <div class="ppppen"><p>от</p> <h3>7 150</h3> <p>руб/сут.</p></div>
-                        <div class="block__underblock"></div>
-    
-                    </div> -->
-                <!-- </div> -->
             </div>
             </div>
         </div>
